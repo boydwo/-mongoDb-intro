ee414c7ce6a1db710e0911c01062a48b77ae2f1d
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2021-02-05 13:24:31 -0300

    find and remove

--- orm/mongo.js
+++ orm/mongo.js
@@ -7,7 +7,15 @@ mongoose.connect('mongodb://localhost/mongo-orm', {useMongoClient:true}).then(()
     cargo:String
   })
   const Pessoa = mongoose.model('Pessoa', PessoasSchema)
-  const marcos = new Pessoa({nome: 'Marcos Tulio', cargo:'CTO'})
-  marcos.save(()=> console.log('salvo'))
+  // const marcos = new Pessoa({nome: 'Marcos Tulio', cargo:'CTO'})
+  // marcos.save(()=> console.log('salvo'))
+
+  Pessoa.find({}, (err, docs)=>{
+    console.log(docs)
+  })
+
+  Pessoa.remove({
+    _id:'601d6fe547a4be242ee93939'
+  }, (err, res) => console.log('ok'))
 
 })
